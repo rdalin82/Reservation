bcaada95591f7a7934d367b70d4fe586a734c56a
rdalin82 <Robert.Dalin@gmail.com>
2016-06-22 22:16:37 -0500

    seats

--- db/migrate/20160618220051_create_reservations.rb
+++ db/migrate/20160618220051_create_reservations.rb
@@ -3,6 +3,7 @@ class CreateReservations < ActiveRecord::Migration
     create_table :reservations do |t|
       t.string :name
       t.integer :time
+      t.integer :seats
       t.timestamps null: false
     end
   end
--- db/migrate/20160623031451_add_seats_to_reservation.rb
+++
@@ -1,5 +0,0 @@
-class AddSeatsToReservation < ActiveRecord::Migration
-  def change
-    add_column :reservations, :seats, :integer
-  end
-end
