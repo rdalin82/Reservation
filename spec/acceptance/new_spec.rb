afeb5cab453ef27190933990b47983380756cd00
rdalin82 <Robert.Dalin@gmail.com>
2016-06-24 18:43:42 -0500

    added more acceptance tests

--- spec/acceptance/new_spec.rb
+++ spec/acceptance/new_spec.rb
@@ -11,9 +11,28 @@ describe "visit new page path", :type => :feature do
       fill_in("reservation[name]", :with=>"John")
       select(1, :from=>"reservation[seats]")
       fill_in("reservation[date]", :with=>"2016-09-22")
-      select("6 AM", :from=>"date[hour]")
+      select("06 AM", :from=>"date[hour]")
       click_button("Reserve")
     end
     expect(page).to have_content "John"
+  end
+  it "should have error messages" do 
+    visit "/reservation/new"
+    within("form") do 
+      click_button("Reserve")
+    end
+    expect(page).to have_content "can't be blank"
+  end
+  it "should have an error for no tables available" do 
+    20.times { |x| Table.create(time: 1, size: 4, date: "2018-09-22") }
+    visit "reservation/new"
+    within("form") do 
+      fill_in("reservation[name]", :with=>"John")
+      select(1, :from=>"reservation[seats]")
+      fill_in("reservation[date]", :with=>"2018-09-22")
+      select("01 AM", :from=>"date[hour]")
+      click_button("Reserve")
+    end
+    expect(page).to have_content "No tables available" 
   end
 end
